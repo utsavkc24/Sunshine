30fe2c8b0a708d7fcd1eee8ba5ac7f642d812daf
Utsav kumar <utsavkc24@gmail.com>
2020-10-12 19:21:29 +0530

    Add map and sharing

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,10 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -21,12 +24,35 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intentThatStartedThisActivity = getIntent();
 
-        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
         if (intentThatStartedThisActivity != null) {
             if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
                 mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
                 mWeatherDisplay.setText(mForecast);
             }
         }
+    }
+
+    // COMPLETED (4) Display the menu and implement the forecast sharing functionality
+    /**
+     * Uses the ShareCompat Intent builder to create our Forecast intent for sharing. We set the
+     * type of content that we are sharing (just regular text), the text itself, and we return the
+     * newly created Intent.
+     *
+     * @return The Intent to use to start our share.
+     */
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(mForecast + FORECAST_SHARE_HASHTAG)
+                .getIntent();
+        return shareIntent;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForecastIntent());
+        return true;
     }
 }
