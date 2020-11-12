b9b09062004c45c68a00241aa6401cc67c63ae3e
Utsav kumar <utsavkc24@gmail.com>
2020-11-12 20:44:44 +0530

    Create the SettingActivity in Sunshine

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -54,4 +54,16 @@ public class DetailActivity extends AppCompatActivity {
         menuItem.setIntent(createShareForecastIntent());
         return true;
     }
+
+    // TODO (7) Launch SettingsActivity when the Settings option is clicked
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setting_details){
+            Intent intent = new Intent(DetailActivity.this, SettingActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
